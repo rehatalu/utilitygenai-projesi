ef59efecd6a69cc5e9e980f3ed492a1ceefa02aa
rehatalu <subscriptionsstartup@gmail.com>
2025-11-10 13:41:37 +0300

    Revert to single-language base without next-intl

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Çalışan Temele Geri Dönüldü!</h1>
+      <p>Adım 14 başarılı. Dil desteği (i18n) kaldırıldı.</p>
+    </main>
+  );
+}
+
